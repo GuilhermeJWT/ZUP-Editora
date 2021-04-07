6a29a0de2bf66e286d8a6ed8394e312104aeb959
Guilherme Joaquim <Guilherme Joaquim@ZUP-4277.local>
2021-04-07 19:09:51 -0300

    Adicionando Alguns métodos para utilizar no Cadastro

--- editora-zup/src/br/com/zup/conexao/Banco.java
+++ editora-zup/src/br/com/zup/conexao/Banco.java
@@ -1,6 +1,7 @@
 package br.com.zup.conexao;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import br.com.zup.model.ModelAutor;
@@ -13,6 +14,36 @@ public class Banco {
 	public void adiciona(ModelAutor modelAutor) {
 		modelAutor.setId(Banco.idSequencial++);
 		listaAutores.add(modelAutor);
+	}
+	
+	public ModelAutor autorFindById(Integer id) {
+		
+		for (ModelAutor modelAutor : listaAutores) {
+			if(modelAutor.getId() == id) {
+				return modelAutor;
+			}
+		}
+		return null;
+		
+	}
+	
+	public List<ModelAutor> getAutores(){
+		return Banco.listaAutores;
+	}
+	
+	public void removerAutor(Integer id) {
+		
+		Iterator<ModelAutor> repetidor = listaAutores.iterator();
+		
+		while(repetidor.hasNext()) {
+			ModelAutor modelAutor = repetidor.next();
+			
+			if(modelAutor.getId() == id) {
+				repetidor.remove();
+			}
+			
+		}
+		
 	}
 
 }
